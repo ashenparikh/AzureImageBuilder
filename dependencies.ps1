a4d593655bf54ea570397cd564a18ccc9fc5c115
Ashen Parikh MS <ashenparikh@microsoft.com>
2022-06-02 16:31:15 -0700

    setting up repo with pre-req dependencies before azure image builder deployment

---
+++ dependencies.ps1
@@ -0,0 +1,17 @@
+#
+Select-AzSubscription -SubscriptionName 'Visual Studio'
+Set-AzContext -SubscriptionId "xxxx-xxxx-xxxx-xxxx"
+
+# setup role def names, these need to be unique
+$timeInt=$(get-date -UFormat "%s")
+$imageRoleDefName="Azure Image Builder Image Def"+$timeInt
+$identityName="aibIdentity"+$timeInt
+
+## Add AZ PS modules to support AzUserAssignedIdentity and Az AIB
+'Az.ImageBuilder', 'Az.ManagedServiceIdentity' | ForEach-Object {Install-Module -Name $_ -AllowPrerelease}
+
+# create identity
+New-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $identityName
+
+$identityNameResourceId=$(Get-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $identityName).Id
+$identityNamePrincipalId=$(Get-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $identityName).PrincipalId
