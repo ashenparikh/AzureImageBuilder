0aff9b2ceab8a0e39a755ea93a24f4c7b57e9c1a
Ashen Parikh MS <ashenparikh@microsoft.com>
2022-06-09 14:14:33 -0700

    dependencies file manages the deployment of image, a local version of the image has been stored

--- dependencies.ps1
+++ dependencies.ps1
@@ -48,12 +48,17 @@ $location="eastus"
 # image template name
 $imageTemplateName="armTemplateAIB"
 
+# image definition name
+$imageDefName = "win10avd"
+
 # distribution properties object name (runOutput), i.e. this gives you the properties of the managed image on completion
 $runOutputName="sigOutput"
 
 # create resource group
 New-AzResourceGroup -Name $imageResourceGroup -Location $location
 
+#specify compute gallery
+$sigGalleryName= "NewmontEnterpriseComputeGallery"
 
 ## Create User Identity
 
@@ -105,4 +110,20 @@ Invoke-WebRequest -Uri $templateUrl -OutFile $templateFilePath -UseBasicParsing
 ((Get-Content -path $templateFilePath -Raw) -replace '<sharedImageGalName>',$sigGalleryName) | Set-Content -Path $templateFilePath
 ((Get-Content -path $templateFilePath -Raw) -replace '<region1>',$location) | Set-Content -Path $templateFilePath
 ((Get-Content -path $templateFilePath -Raw) -replace '<imgBuilderId>',$identityNameResourceId) | Set-Content -Path $templateFilePath
+
+#Submit the image
+New-AzResourceGroupDeployment -ResourceGroupName $imageResourceGroup -TemplateFile $templateFilePath -TemplateParameterObject @{"api-Version" = "2020-02-14"} -imageTemplateName $imageTemplateName -svclocation $location
+
+#Build the image
+Start-AzImageBuilderTemplate -ResourceGroupName $imageResourceGroup -Name $imageTemplateName -NoWait
+
+$getStatus=$(Get-AzImageBuilderTemplate -ResourceGroupName $imageResourceGroup -Name $imageTemplateName)
+
+# this shows all the properties
+$getStatus | Format-List -Property *
+
+# these show the status the build
+$getStatus.LastRunStatusRunState 
+$getStatus.LastRunStatusMessage
+$getStatus.LastRunStatusRunSubState
 
