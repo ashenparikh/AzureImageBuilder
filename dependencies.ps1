4bb78f18f8f384a3af4d294198b45f4e72d432e4
Ashen Parikh MS <ashenparikh@microsoft.com>
2022-06-28 14:13:26 -0700

    create image def in compute gallery

--- dependencies.ps1
+++ dependencies.ps1
@@ -5,34 +5,34 @@ $subscriptionID="f0e6a708-6962-41b1-b2d3-a43615d7b4fb"
 Select-AzSubscription -SubscriptionName 'CSR-CUSPOC-NMST-ashenparikh-sub02'
 Set-AzContext -SubscriptionId $subscriptionID
 
-# # Register Resource Providers
-# if (Get-AzResourceProvider -ProviderNamespace Microsoft.VirtualMachineImages) {
-#     Write-Output "Resource Provider VirtualMachineImages Already Registered"
-# }
-# else {
-#     Register-AzResourceProvider -ProviderNamespace Microsoft.VirtualMachineImages
-# }
+Register Resource Providers
+if (Get-AzResourceProvider -ProviderNamespace Microsoft.VirtualMachineImages) {
+    Write-Output "Resource Provider VirtualMachineImages Already Registered"
+}
+else {
+    Register-AzResourceProvider -ProviderNamespace Microsoft.VirtualMachineImages
+}
 
-# if (Get-AzResourceProvider -ProviderNamespace Microsoft.Storage)  {
-#     Write-Output "Resource Provider Microsoft.Storage  Already Registered"
-# }
-# else {
-#     Register-AzResourceProvider -ProviderNamespace Microsoft.Storage
-# }
+if (Get-AzResourceProvider -ProviderNamespace Microsoft.Storage)  {
+    Write-Output "Resource Provider Microsoft.Storage  Already Registered"
+}
+else {
+    Register-AzResourceProvider -ProviderNamespace Microsoft.Storage
+}
 
-# if (Get-AzResourceProvider -ProviderNamespace Microsoft.Compute) {
-#     Write-Output "Resource Provider Microsoft.Compute Already Registered"
-# }
-# else {
-#     Register-AzResourceProvider -ProviderNamespace Microsoft.Compute
-# }
+if (Get-AzResourceProvider -ProviderNamespace Microsoft.Compute) {
+    Write-Output "Resource Provider Microsoft.Compute Already Registered"
+}
+else {
+    Register-AzResourceProvider -ProviderNamespace Microsoft.Compute
+}
 
-# if (Get-AzResourceProvider -ProviderNamespace Microsoft.KeyVault) {
-#     Write-Output "Resource Provider KeyVault Already Registered"
-# }
-# else {
-#     Register-AzResourceProvider -ProviderNamespace Microsoft.KeyVault
-# }
+if (Get-AzResourceProvider -ProviderNamespace Microsoft.KeyVault) {
+    Write-Output "Resource Provider KeyVault Already Registered"
+}
+else {
+    Register-AzResourceProvider -ProviderNamespace Microsoft.KeyVault
+}
 
 ##Set up Env Variables 
 
@@ -43,7 +43,7 @@ Import-Module Az.Accounts
 $imageResourceGroup="rsg-usw2-p-vditemplate-01"
 
 # location (see possible locations in main docs)
-$location="westus2"
+$location="eastus"
 
 # image template name
 $imageTemplateName="armTemplateAIB"
@@ -53,6 +53,9 @@ $imageDefName = "win10avd"
 
 # distribution properties object name (runOutput), i.e. this gives you the properties of the managed image on completion
 $runOutputName="sigOutput"
+
+# image name
+$imagename="MasterImage"
 
 # create resource group
 #New-AzResourceGroup -Name $imageResourceGroup -Location $location
@@ -67,25 +70,22 @@ $timeInt=$(get-date -format FileDateTime)
 $imageRoleDefName="Azure Image Builder Image Def"+$timeInt
 $identityName="aibIdentity"+$timeInt
 
-## Add AZ PS modules to support AzUserAssignedIdentity and Az AIB
-# Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+# Add AZ PS modules to support AzUserAssignedIdentity and Az AIB
+Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
 
-# if (Get-Module -Name "Az.ImageBuilder") {
-#     Write-Output "Az.ImageBuilder Already Installed"
-# }
-# else {
-#     Install-Module -Name 'Az.ImageBuilder'
-# }
+if (Get-Module -Name "Az.ImageBuilder") {
+    Write-Output "Az.ImageBuilder Already Installed"
+}
+else {
+    Install-Module -Name 'Az.ImageBuilder'
+}
 
-# if (Get-Module -Name 'Az.ManagedServiceIdentity') {
-#     Write-Output "Az.ManagedServiceIdentity Already Installed"
-# }
-# else {
-#     Install-Module -Name 'Az.ManagedServiceIdentity'
-# }
-
-#Install-Module -Name 'Az.ImageBuilder'
-#Install-Module -Name 'Az.ManagedServiceIdentity'  
+if (Get-Module -Name 'Az.ManagedServiceIdentity') {
+    Write-Output "Az.ManagedServiceIdentity Already Installed"
+}
+else {
+    Install-Module -Name 'Az.ManagedServiceIdentity'
+}
 
 # create identity
 New-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $identityName -Location  $location
@@ -94,7 +94,9 @@ $identityNameResourceId=$(Get-AzUserAssignedIdentity -ResourceGroupName $imageRe
 $identityNamePrincipalId=$(Get-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $identityName).PrincipalId
 
 
-## Create Role Definition for Identity
+New-AzGalleryImageDefinition -GalleryName $sigGalleryName -ResourceGroupName $imageResourceGroup -Location $location -Name $imageDefName -OsState generalized -OsType Windows -Publisher 'MicrosoftWindowsDesktop' -Offer 'windows-10' -Sku '20h2-evd'
+
+# ## Create Role Definition for Identity
 $aibRoleImageCreationUrl="https://raw.githubusercontent.com/azure/azvmimagebuilder/master/solutions/12_Creating_AIB_Security_Roles/aibRoleImageCreation.json"
 $aibRoleImageCreationPath = "aibRoleImageCreation.json"
 
@@ -128,14 +130,14 @@ Invoke-WebRequest -Uri $templateUrl -OutFile $templateFilePath -UseBasicParsing
 ((Get-Content -path $templateFilePath -Raw) -replace '<region1>',$location) | Set-Content -Path $templateFilePath
 ((Get-Content -path $templateFilePath -Raw) -replace '<imgBuilderId>',$identityNameResourceId) | Set-Content -Path $templateFilePath
 
-#Submit the image
-New-AzResourceGroupDeployment -ResourceGroupName $imageResourceGroup -TemplateFile $templateFilePath -imageTemplateName $imageTemplateName -svclocation $location
+# #Submit the image
+New-AzResourceGroupDeployment -ResourceGroupName $imageResourceGroup -TemplateFile $templateFilePath -TemplateParameterObject @{"api-Version" = "2020-02-14"} -imageTemplateName $imageTemplateName -svclocation $location
 # Optional - if you have any errors running the above, run:
 $getStatus=$(Get-AzImageBuilderTemplate -ResourceGroupName $imageResourceGroup -Name $imageTemplateName)
 $getStatus.ProvisioningErrorCode 
 $getStatus.ProvisioningErrorMessage
 
-#Build the image
+#Build the image Definition
 Start-AzImageBuilderTemplate -ResourceGroupName $imageResourceGroup -Name $imageTemplateName -NoWait
 
 $getStatus=$(Get-AzImageBuilderTemplate -ResourceGroupName $imageResourceGroup -Name $imageTemplateName)
@@ -148,3 +150,37 @@ $getStatus.LastRunStatusRunState
 $getStatus.LastRunStatusMessage
 $getStatus.LastRunStatusRunSubState
 
+# #Create Image
+
+# $imageConfig = New-AzImageConfig -Location 'East US';
+# $osDiskVhdUri = "https://contoso.blob.core.windows.net/test/os.vhd"
+# $dataDiskVhdUri1 = "https://contoso.blob.core.windows.net/test/data1.vhd"
+# $dataDiskVhdUri2 = "https://contoso.blob.core.windows.net/test/data2.vhd"
+# Set-AzImageOsDisk -Image $imageConfig -OsType 'Windows' -OsState 'Generalized' -BlobUri $osDiskVhdUri;
+# Add-AzImageDataDisk -Image $imageConfig -Lun 1 -BlobUri $dataDiskVhdUri1;
+# Add-AzImageDataDisk -Image $imageConfig -Lun 2 -BlobUri $dataDiskVhdUri2;
+# New-AzImage -Image $imageConfig -ImageName $imagename -ResourceGroupName $imageResourceGroup;
+
+
+
+# #Create Image Version
+# $region1 = @{Name='East US';ReplicaCount=1}
+# $region2 = @{Name='West US 2';ReplicaCount=1}
+# $targetRegions = @($region1,$region2)
+# $sourceimageid ="/subscriptions/$subscriptionID/resourceGroups/$imageResourceGroup/providers/Microsoft.Compute/images/$imageDefName"
+
+# $imageversionjob = New-AzGalleryImageVersion `
+#    -GalleryImageDefinitionName $imageDefName`
+#    -GalleryImageVersionName '1.0.0' `
+#    -GalleryName $imagename `
+#    -ResourceGroupName $imageResourceGroup `
+#    -Location $location `
+#    -TargetRegion $targetRegions  `
+#    -SourceImageId $sourceimageid `
+#    -PublishingProfileEndOfLifeDate '2024-12-01' `
+#    -asJob
+
+# $imageversionjob.State 
+
+# #Create Session Host
+# #Get-AzImage -ResourceGroupName $imageResourceGroup -ImageName $imageTemplateName 
